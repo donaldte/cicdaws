59f22e68bdecbad46e9091f1827551a85c2b10a4
donaldte <donaldtedom0@gmail.com>
2025-10-19 22:16:24 +0100

    feat: refactor configuration

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["16.171.197.62"]/' /home/ubuntu/project_devops/agency/settings.py
+sed -i 's/\[]/\["3.144.212.231"]/' /home/ubuntu/project_devops/agency/settings.py
 
 python manage.py migrate 
 python manage.py makemigrations     
